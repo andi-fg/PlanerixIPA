552327432664125472b3eebd1e573d4773d188d9
mischa-fg <Fischer@fuchsgroup.ch>
2022-03-24 14:43:33 +0100

    Login hinzugefügt

---
+++ PlanerixIPA/Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PlanerixIPA.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanerixIPA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly PlanerixContext _context;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(PlanerixContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(Benutzer b)
+        {
+            //Benutzer mit Benutzernamen suchen
+            var benutzer = _context.Benutzers.Where(benutzer => benutzer.Benutzername.Equals(b.Benutzername)).FirstOrDefault();
+            if (benutzer == null)
+            {
+                return new BadRequestObjectResult("Logindaten ungültig.");
+            }
+            //Passwort hashen und überprüfen
+            var hashedPasswort = GetHash(b.Passwort, benutzer.Salt);
+            if (!hashedPasswort.Equals(benutzer.Passwort))
+            {
+                return new BadRequestObjectResult("Logindaten ungültig.");
+            }
+            //Logindaten OK Token generieren und zurückgeben
+            var authClaims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Iss, _configuration["Jwt:ValidIssuer"]),
+                new Claim(JwtRegisteredClaimNames.Email, benutzer.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+            
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(10),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+             );
+            var t = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new
+            {
+                token = t,
+                expiration = token.ValidTo
+            });
+        }
+        //Passwort hashen mit bestehenden Salt
+        public static string GetHash(string pw, byte[] salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(pw, salt, KeyDerivationPrf.HMACSHA1, 10000, 32));
+        }
+    }
+}
---
+++ PlanerixIPA/wwwroot/js/Login.js
@@ -0,0 +1,32 @@
+﻿function login() {
+    var fehlerMeldung = document.getElementById("fehler");
+    fehlerMeldung.innerHTML = "";
+    //Daten aus den Inputfeldern lesen
+    var benutzer = {
+        benutzername : document.getElementById("benutzername").value,
+        passwort : document.getElementById("passwort").value
+    }
+    //Login abfrage machen
+    var uri = "api/login";
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(benutzer)
+    })
+        .then(response => {
+            if (response.status == "200") {
+                return response.json();
+            } else {
+                response.text().then(data => { fehlerMeldung.innerHTML = data.replace(/\"/g, "") });
+                throw new Error("HTTP status " + response.status);
+            }
+        })
+        .then(data => {
+            sessionStorage.setItem("tokenKey", data.token);
+            window.location = "Geburtstagsliste.html";
+        })
+        .catch(error => console.error('Unable to Login.', error));
+}
