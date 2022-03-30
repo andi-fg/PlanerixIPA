5a0225e5c2d5a4ef271cd116e1c563500cee7cc3
mischa-fg <Fischer@fuchsgroup.ch>
2022-03-30 13:42:09 +0200

    Authorize hinzugefügt bei EinAustrittmonat

--- PlanerixIPA/Controllers/EinAustrittmonatController.cs
+++ PlanerixIPA/Controllers/EinAustrittmonatController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PlanerixIPA.Models;
 using PlanerixIPA.Models.ViewModels;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace PlanerixIPA.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class EinAustrittmonatController : ControllerBase
